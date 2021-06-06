f206c13239369682ef8737797730dbcb75860458
Julia Nosal <julianosal@student.agh.edu.pl>
2021-06-06 16:33:46 +0200

    index.html initial commit

--- google/vue.js
+++ google/vue.js
@@ -1,6 +1,24 @@
-var wrapper = new Vue({
-    el: '#wrapper',
+var app = new Vue({
+    el: "#wrapper",
     data: {
         googleSearch: "",
-    }
+        cities: window.cities,
+        isActive: 0,
+        control: 0
+    },
+
+    computed: {
+        filteredCities: function () {
+            if (this.googleSearch.length > 0) {
+                let miasta = this.cities.filter(city => city.name.includes(this.googleSearch))
+                if (miasta.length > 10) {
+                    return miasta.slice(0, 10);
+                }
+                else {
+                    return miasta;
+                }
+            }
+        }
+    },
+
 });
